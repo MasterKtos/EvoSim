de4c2830619799d4d075a42b5ad3bb9d83b54073
bortok1 <94564592+bortok1@users.noreply.github.com>
2023-10-25 01:29:02 +0200

    FOV Creatures In Sight

--- Source/EvoSim/Creature/CreatureComponents/FovComponent.cpp
+++ Source/EvoSim/Creature/CreatureComponents/FovComponent.cpp
@@ -4,10 +4,8 @@
 #include "FovComponent.h"
 
 #include "ComponentReregisterContext.h"
-#include "VectorTypes.h"
 #include "Components/SphereComponent.h"
 #include "EvoSim/Creature/Creature.h"
-#include "EvoSim/Map/MapManager.h"
 #include "EvoSim/Map/Tile.h"
 #include "Kismet/KismetSystemLibrary.h"
 
@@ -23,75 +21,85 @@ void UFovComponent::BeginPlay()
 	Owner = Cast<ACreature>(GetOwner());
 	if(!ensure(Owner))
 		UE_LOG(LogActorComponent, Log, TEXT("FovComponent | No Owner (or owner is not a Creature)."));
+
+	IgnoredActors.Add(Owner);
+}
+
+TArray<AActor*> UFovComponent::GetOverlappingActors(UClass* ActorsOfClass) const
+{
+	TArray<AActor*> OverlappingActors;
+	UKismetSystemLibrary::ComponentOverlapActors(Owner->FovSphereComponent, Owner->GetTransform(),
+		TraceObjectTypes, ActorsOfClass, IgnoredActors, OverlappingActors);
+	return OverlappingActors;
 }
 
 void UFovComponent::UpdateTilesInSight()
 {
-	if(Owner->MapManager->Tiles.IsEmpty())
-		return;
-	
 	WaterTiles.Empty();
 	PlantTiles.Empty();
-	MeatTiles.Empty();
+	CreaturesInSight.Empty();
 	
-	const FVector OwnerLocation3D = Owner->GetActorLocation();
-	const FVector2D OwnerLocation = FVector2D(OwnerLocation3D.X, OwnerLocation3D.Y);
-	
-	TArray<AActor*> IgnoredActors;
-	IgnoredActors.Add(Owner);
-	
-	TArray<AActor*> OverlappingTiles;
-	UKismetSystemLibrary::ComponentOverlapActors(Owner->FovSphereComponent, Owner->GetTransform(),
-		TraceObjectTypes, ATile::StaticClass(), IgnoredActors, OverlappingTiles);
+	const FVector OwnerLocation = Owner->GetActorLocation();
 
-	// TArray<AActor*> OverlappingCreatures;
-	// if(Owner->IsHerbivorous)
-	// {
-		// UKismetSystemLibrary::ComponentOverlapActors(Owner->FovSphereComponent, Owner->GetTransform(),
-		// TraceObjectTypes, Owner->GetClass(), IgnoredActors, OverlappingCreatures);
-	// }
-	
-	for (AActor* ActorTile : OverlappingTiles)
+	TArray<ATile*> OverlappingTiles;
+
 	{
-		ATile* Tile = Cast<ATile>(ActorTile);
-		
-		// Is correct type?
-		if(Tile->Type != ETileType::Water && Tile->Type != ETileType::Plant)
-			continue;
-			
-		const FVector TileLocation3D = Tile->GetActorLocation();
-		const FVector2D TileLocation = FVector2D(TileLocation3D.X, TileLocation3D.Y);
-
-		// Is correct distance away?
-		if(UE::Geometry::Distance(TileLocation, OwnerLocation) > Owner->ViewDistance * 100)
-			continue;
-
-		// Is in field of view?
-		FVector CreatureTileVector = (OwnerLocation3D - TileLocation3D).GetSafeNormal();
-		FVector CreatureForwardVector = Owner->GetActorForwardVector();
-
-		float DotProduct = FVector::DotProduct(CreatureTileVector, CreatureForwardVector);
-		FVector CrossProduct = FVector::CrossProduct(CreatureTileVector, CreatureForwardVector);
-		float CrossProductMagnitude = CrossProduct.Size();
-
-		float AngleInRadians = FMath::Atan2(CrossProductMagnitude, DotProduct);
-		float AngleInDegrees = FMath::RadiansToDegrees(AngleInRadians);
-
-		if(AngleInDegrees * 2 > Owner->FieldOfView)
+		TArray<AActor*> OverlappingTilesActors = GetOverlappingActors(ATile::StaticClass());
+		for(AActor* TileActor : OverlappingTilesActors)
+		{
+			OverlappingTiles.Add(Cast<ATile>(TileActor));
+		}
+	
+		TArray<AActor*> OverlappingCreaturesActors = GetOverlappingActors(ACreature::StaticClass());
+		for(AActor* CreatureActor : OverlappingCreaturesActors)
+		{
+			CreaturesInSight.Add(Cast<ACreature>(CreatureActor));
+		}
+	}
+	
+	for (ATile* Tile : OverlappingTiles)
+	{
+		if(Tile->Type == ETileType::Obstacle)
 			continue;
 		
-		// Is obstacle in the way?
+		// Is obstacle in the line of sight?
+		static const FVector TileLocation = Tile->GetActorLocation();
+		
 		FHitResult HitResult;
-		FVector End = TileLocation3D - FVector(0,0,30);
-		FVector Start = OwnerLocation3D - FVector(0,0,30);
+		static FVector End = TileLocation - FVector(0,0,30);
+		static FVector Start = OwnerLocation;
 		Start.Z = End.Z;
 		
 		if (GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_WorldStatic, FCollisionQueryParams()))
 			continue;
 
 		DrawDebugLine(GetWorld(), Start, End, FColor::Red, false, 1.0f, 0, 5);
+		
+		// Is in field of view?
+		static FVector CreatureTileVector = (OwnerLocation - TileLocation).GetSafeNormal();
+		static FVector CreatureForwardVector = Owner->GetActorForwardVector();
 
-		// Tile is in field of view.
+		static float DotProduct = FVector::DotProduct(CreatureTileVector, CreatureForwardVector);
+		static float CrossProductMagnitude = FVector::CrossProduct(CreatureTileVector, CreatureForwardVector).Size();
+
+		static float AngleInDegrees = FMath::RadiansToDegrees(FMath::Atan2(CrossProductMagnitude, DotProduct));
+
+		if(AngleInDegrees * 2 > Owner->FieldOfView)
+		{
+			// Check if on any of the Tiles that are removed from the pool, 
+			// were any creatures since it's less than 50% of all
+			for(ACreature* Creature : CreaturesInSight)
+			{
+				if(Creature->CurrentTile == Tile)
+					CreaturesInSight.Remove(Creature);
+			}
+			continue;
+		}
+
+		// Tile is in field of view. Check it's type.
+		if(Tile->Type == ETileType::Land)
+			continue;
+		
 		if(Tile->Type == ETileType::Water)
 		{
 			WaterTiles.Add(Tile);
@@ -113,8 +121,8 @@ const TArray<ATile*>& UFovComponent::GetPlantTilesInSight() const
 	return PlantTiles;
 }
 
-const TArray<ATile*>& UFovComponent::GetMeatInSight() const
+const TArray<ACreature*>& UFovComponent::GetCreaturesInSight() const
 {
-	return MeatTiles;
+	return CreaturesInSight;
 }
 
